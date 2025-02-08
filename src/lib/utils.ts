bf1b1e4da2335da3d61bfa7dff231b92a104c169
mishael joseph <mishaeljoe55@gmail.com>
2025-02-08 07:16:19 +0100

    updated shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
